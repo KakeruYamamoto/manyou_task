ca3a60f395ecdeb11eeca40fdf31071102901e9f
kakeru <okinawan.kkr0713@icloud.com>
2019-11-12 16:45:45 +0900

    systemとfactories追記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    # 下記の内容は実際に作成するカラム名に合わせて変更してください
+  title { 'test_title' }
+  content { 'test_content' }
+
+
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe Task, type: :system do
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示されること'
+      # テストで使用するためのタスクを作成
+        task = FactoryBot.create(:task, task_name: 'task')
+
+        # タスク一覧ページに遷移
+        visit tasks_path
+
+        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+        # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）
+        expect(page).to have_content 'task'
+
+        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
     end
   end
 
