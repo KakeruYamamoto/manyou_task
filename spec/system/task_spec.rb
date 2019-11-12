681d7268273bdc38cfb8611723d74ece707e9076
kakeru <okinawan.kkr0713@icloud.com>
2019-11-12 16:25:38 +0900

    system/task_spec.rb作成

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :system do
+  describe 'タスク一覧画面' do
+    context 'タスクを作成した場合' do
+      it '作成済みのタスクが表示されること'
+    end
+  end
+
+  describe 'タスク登録画面' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存されること'
+    end
+  end
+
+  describe 'タスク詳細画面' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示されたページに遷移すること'
+     end
+  end
+end
