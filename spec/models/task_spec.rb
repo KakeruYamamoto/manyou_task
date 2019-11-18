450356cc3db08f36cd6a574f7f17219f1b21a115
kakeru <okinawan.kkr0713@icloud.com>
2019-11-18 20:12:49 +0900

    models/task_specに追記

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "titleが空ならバリデーションが通らない" do
+    task = Task.new(task_name: '', task_content: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    task = Task.new(task_name: '失敗テスト', task_content: '')
+    expect(task).not_to be_valid
+  end
+
+  it "titleとcontentに内容が記載されていればバリデーションが通る" do
+    task = Task.new(task_name: '成功テスト', task_content: '成功テスト')
+    expect(task).to be_valid
+  end
 end
