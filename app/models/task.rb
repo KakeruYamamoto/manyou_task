06da800933609a5589ea0903f6a3243cf0f9bb73
kakeru <okinawan.kkr0713@icloud.com>
2019-11-18 15:38:05 +0900

    default_scope を追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   # belongs_to :user
-
+  default_scope -> { order(created_at: :desc) }
   validates :task_name, presence: true
   validates :task_content, presence: true
 end
