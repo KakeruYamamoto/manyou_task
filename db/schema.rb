a282358975b981c6c41479ae9b7be71e775a27bc
kakeru <okinawan.kkr0713@icloud.com>
2019-11-19 17:15:20 +0900

    カラム追加,スキーマ更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_045926) do
+ActiveRecord::Schema.define(version: 2019_11_19_070223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_11_14_045926) do
     t.text "task_content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline"
+    t.integer "priority"
+    t.string "status"
+    t.string "task_label"
   end
 
 end
