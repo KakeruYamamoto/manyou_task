a404fbb98a1204415a96222e28d0143d45b44331
kakeru <okinawan.kkr0713@icloud.com>
2019-11-25 14:50:47 +0900

    スキーマ更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_045926) do
+ActiveRecord::Schema.define(version: 2019_11_25_044641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,19 @@ ActiveRecord::Schema.define(version: 2019_11_14_045926) do
   create_table "tasks", force: :cascade do |t|
     t.string "task_name", null: false
     t.text "task_content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deadline"
+    t.integer "priority"
+    t.string "status"
+    t.string "task_label"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
