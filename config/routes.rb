b9c2053a440bf65e8aaba20d3df87afd4b286055
kakeru <okinawan.kkr0713@icloud.com>
2019-11-25 18:00:16 +0900

    routing,レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users
   root to: "tasks#index"
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
