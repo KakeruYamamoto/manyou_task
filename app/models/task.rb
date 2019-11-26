6ce6e0b8da290c2acbb218ad634d8bea5eebe699
kakeru <okinawan.kkr0713@icloud.com>
2019-11-26 12:39:16 +0900

    model変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :task_name, presence: true
   validates :task_content, presence: true
