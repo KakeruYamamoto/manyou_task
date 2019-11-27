cab991376ddbf154d5781147fb8f13946db82e18
kakeru <okinawan.kkr0713@icloud.com>
2019-11-27 15:57:36 +0900

    systemファイル作成

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,4 +93,15 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
 end
