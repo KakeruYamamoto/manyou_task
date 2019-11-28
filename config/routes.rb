3ec80e7c165a156ab83dde60abe768031c509438
kakeru <okinawan.kkr0713@icloud.com>
2019-11-28 17:55:29 +0900

    routes.rb に　namespace :admin　追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit]
   resources :tasks
+  namespace :admin do
+    resources :users
+  end
 end
