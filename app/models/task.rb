c31ea5fcd85879fb77d242426bb15e17ca1f2456
kakeru <okinawan.kkr0713@icloud.com>
2019-12-02 15:13:07 +0900

    label.labeling,task関連付

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,6 @@ class Task < ApplicationRecord
   enum priority:[ :高, :中, :低]
   validates :task_name, presence: true
   validates :task_content, presence: true
-
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
 end
