cde41233f01cc0ff77700f4b950256470a914eb5
kakeru <okinawan.kkr0713@icloud.com>
2020-04-15 22:24:48 +0900

    ゲストログイン機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,14 @@ class SessionsController < ApplicationController
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
   end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com', user_name: 'ゲストユーザー') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.created_at = Time.now
+    end
+    session[:user_id] = user.id
+    redirect_to tasks_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@
 Rails.application.routes.draw do
   resources :labels
   root to: 'sessions#new'
-  resources :sessions, only: %i[new create destroy]
+  resources :sessions, only: %i[new create destroy new_guest] do
+    collection do
+      post :new_guest, as: 'guest_sign_in'
+    end
+  end
   resources :users
   resources :tasks
   namespace :admin do
