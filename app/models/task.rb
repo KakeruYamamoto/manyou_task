3daad22044f132ce779558d5460f621fc47a4ae9
kakeru <okinawan.kkr0713@icloud.com>
2020-04-15 19:31:10 +0900

    fix

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  belongs_to :user
-  enum priority: %i[高 中 低]
   validates :task_name, presence: true
   validates :task_content, presence: true
+  enum priority: %i[高 中 低]
+  belongs_to :user
   has_many :labellings, dependent: :destroy
   has_many :labels, through: :labellings
 end
--- app/views/users/_user.json.jbuilder
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-json.extract! user, :id, :user_name, :email, :password_digest, :admin, :created_at, :updated_at
-json.url user_url(user, format: :json)
